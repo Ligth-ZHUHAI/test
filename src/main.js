526577664612f0611e512b6bf20ca7f36fb36c29
Light <382479818@qq.com>
2020-04-06 02:12:36 +0800

    登录页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+// 注册elementUI
+Vue.use(ElementUI);
 
 
 // 导入路由对象
--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -5,6 +5,32 @@
             <span class="title">黑马面面</span>
             <span class="line">|</span>
             <span class="userLogin">用户登录</span>
+            <el-form ref="form" :model="form" label-width="0px" class="loginForm">
+                <el-form-item>
+                    <el-input placeholder="请输入手机号" prefix-icon="el-icon-user"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input placeholder="请输入密码" prefix-icon="el-icon-lock"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-row>
+                        <el-col :span="18">
+                            <el-input placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+            </el-form>
+            <div class="terms">
+                <el-checkbox v-model="checked">
+                    <span class="content">我已阅读并同意<a href="#">用户协议</a>和 <a href="#">隐私条款</a></span>
+                </el-checkbox>
+            </div>
+            <div class="loginButton">
+                <el-button type="primary" class="button">登录</el-button>
+            </div>
+            <div class="registerButton">
+                <el-button type="primary" class="button">注册</el-button>
+            </div>
         </div>
         <div class="right">
             <img src="@/assets/img/login_banner_ele.png" alt="">
@@ -58,6 +84,34 @@
                 color: rgba(12, 12, 12, 1);
                 margin-left: 15px;
             }
+
+            .loginForm {
+                margin-top: 30px;
+            }
+
+            .terms {
+                margin-bottom: 30px;
+
+                .content {
+                    font-size: 14px;
+                    font-weight: 400;
+                    color: rgba(153, 153, 153, 1);
+                    line-height: 16px;
+
+                    a {
+                        color: #3296FA
+                    }
+                }
+            }
+
+            .loginButton {
+                margin-bottom: 20px;
+            }
+
+            .button {
+                width: 394px;
+            }
+
         }
     }
 </style>
